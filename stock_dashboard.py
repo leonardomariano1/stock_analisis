3105594be489182494705f10353843ecf8f14315
leonardomariano1 <143562678+leonardomariano1@users.noreply.github.com>
2024-01-09 01:16:47 -0300

    Update stock_dashboard.py

--- stock_dashboard.py
+++ stock_dashboard.py
@@ -166,4 +166,6 @@ app.layout = html.Div([
 # Run the Dash app
 
 if __name__ == '__main__':
-   app.run_server(debug=True, port=int(os.environ.get('PORT', 8050)))
+   port = int(os.environ.get('PORT', 8050))
+   app.run_server(debug=True, port=port)
+
